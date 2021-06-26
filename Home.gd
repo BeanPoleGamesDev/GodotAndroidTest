efcb835054a122072e62fd20449f7d6050bbd808
opsaaaaa <sean@ferney.org>
2021-06-26 09:30:55 -0600

    init

---
+++ Home.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+var taps = 0
+
+onready var count_node = $Center/VBox/Count
+onready var anim = $Anim
+
+
+func _input(event):
+	print(event)
+	match event.get_class():
+		'InputEventScreenTouch':
+			set_taps(taps + 1)
+
+func set_taps(val)->void:
+	taps = val
+	count_node.text = "%04d" % taps
+	anim.play("tap")
+	
